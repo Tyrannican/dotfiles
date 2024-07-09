ecaf671d4c1b8404d05626704e6640b10db6a59e
Graham Keenan <graham.keenan@outlook.com>
2024-07-09 16:00:31 +0100

    feat(nvim): moved stuff around

---
+++ editor/nvim/plugin/terminal.lua
@@ -0,0 +1,24 @@
+local set = vim.opt_local
+
+-- Local settings for terminals
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup('custom-term-open', {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+    vim.bo.filetype = 'terminal'
+  end,
+})
+
+-- Escape in the terminal
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
+
+-- Open a terminal at the bottom of the screen with a fixed height
+vim.keymap.set('n', '<leader>st', function()
+  vim.cmd.new()
+  vim.cmd.wincmd 'J'
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
