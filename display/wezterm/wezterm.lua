f5f30a476fa30b289f2c74b12742332b159347c3
Graham Keenan <graham.keenan@outlook.com>
2024-07-21 14:21:22 +0100

    updated wezterm keybinds

--- display/wezterm/wezterm.lua
+++ display/wezterm/wezterm.lua
@@ -5,6 +5,22 @@ local config = wezterm.config_builder()
 config.color_scheme = 'carbonfox'
 config.font_size = 16.0
 config.window_decorations = 'NONE'
-config.front_end = "WebGpu"
+config.keys = {
+  {
+    key = 'RightArrow',
+    mods = 'SHIFT',
+    action = wezterm.action { ActivateTabRelative = 1 }
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'SHIFT',
+    action = wezterm.action { ActivateTabRelative = -1 }
+  },
+  {
+    key = 'R',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ReloadConfiguration
+  }
+}
 
 return config
