e36d256fd53aac61625b086a3af4da131e10bf82
Graham Keenan <graham.keenan@outlook.com>
2024-08-03 19:14:07 +0100

    wezterm font size decreased

--- display/wezterm/wezterm.lua
+++ display/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'carbonfox'
-config.font_size = 16.0
+config.font_size = 14.0
 config.window_decorations = 'NONE'
 config.keys = {
   {
