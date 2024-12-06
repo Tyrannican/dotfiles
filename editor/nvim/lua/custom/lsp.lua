4c4820a86ddd6980eba563b2412c6dd346184972
Graham Keenan <graham.keenan@outlook.com>
2024-12-06 16:51:56 +0000

    nvim: updated lua lsp

--- editor/nvim/lua/custom/lsp.lua
+++ editor/nvim/lua/custom/lsp.lua
@@ -78,10 +78,7 @@ local servers = {
         runtime = { version = "LuaJIT" },
         workspace = {
           checkThirdParty = false,
-          library = {
-            '${3rd}/luv/library',
-            unpack(vim.api.nvim_get_runtime_file('', true)),
-          },
+          library = vim.api.nvim_get_runtime_file('', true),
         },
         completion = {
           callSnippet = 'Replace'
