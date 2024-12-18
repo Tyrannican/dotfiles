f85fd130861daf5e088c639936fc4002d13cd8fb
Graham Keenan <graham.keenan@outlook.com>
2024-12-18 15:46:31 +0000

    something

--- editor/nvim/lua/custom/lsp.lua
+++ editor/nvim/lua/custom/lsp.lua
@@ -72,7 +72,7 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers {
   function(server_name)
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    capabilities = require('blink.cmp').get_lsp_capabilities()
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
