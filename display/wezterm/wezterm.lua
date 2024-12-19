906d881a26c46cfd79cb054cbbb111bc87354b4b
Graham Keenan <graham.keenan@outlook.com>
2024-12-19 18:58:17 +0000

    wezterm updates

--- display/wezterm/wezterm.lua
+++ display/wezterm/wezterm.lua
@@ -5,6 +5,9 @@ local config = wezterm.config_builder()
 config.color_scheme = 'carbonfox'
 config.font_size = 14.0
 config.window_decorations = 'NONE'
+config.front_end = "WebGpu"
+config.webgpu_preferred_adapter = wezterm.gui.enumerate_gpus()[3]
+
 config.keys = {
   {
     key = 'RightArrow',
