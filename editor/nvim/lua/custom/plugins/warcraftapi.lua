84696a27c0731272b2d03c3cdd6f48fe89bb33a5
Graham Keenan <graham.keenan@outlook.com>
2024-12-27 18:22:54 +0000

    wow plugin dev

---
+++ editor/nvim/lua/custom/plugins/warcraftapi.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "warcraft-api",
+    name = "warcraft-api",
+    dir = "/home/gak/workspace/lua/plugins/warcraft-api.nvim",
+    config = function()
+      require('warcraft-api').setup()
+    end
+  }
+}
