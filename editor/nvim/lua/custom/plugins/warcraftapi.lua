e8892cb0c02fc2b80c98f73c28969d290faf767c
Graham Keenan <graham.keenan@outlook.com>
2024-12-28 00:09:20 +0000

    changed directory

--- editor/nvim/lua/custom/plugins/warcraftapi.lua
+++ editor/nvim/lua/custom/plugins/warcraftapi.lua
@@ -2,7 +2,7 @@ return {
   {
     "warcraft-api",
     name = "warcraft-api",
-    dir = "/home/gak/workspace/lua/plugins/warcraft-api.nvim",
+    dir = "/home/gak/workspace/neovim-plugins/warcraft-api.nvim",
     config = function()
       require('warcraft-api').setup()
     end
